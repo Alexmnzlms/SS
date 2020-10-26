a6e53cf36edcc596b94422a470d68d3a3bdfe581
Alexmnzlms <alexmanza2@gmail.com>
2020-10-26 20:06:54 +0100

    Variación de las veces

--- practica2/script/ejecucion-quiosco.sh
+++ practica2/script/ejecucion-quiosco.sh
@@ -3,29 +3,35 @@
 cd ..
 make
 cd script
-
-for y in 1 5 10; do
-	echo -n "" > "../data/quiosco_${y}_a.data"
-	echo -n "" > "../data/quiosco_${y}_b.data"
-	echo -n "" > "../data/quiosco_${y}_c.data"
+for v in 1 100 1000 5000 10000 100000; do
+	for y in 1 5 10; do
+		echo -n "" > "../data/quiosco_${y}_${v}_a.data"
+		echo -n "" > "../data/quiosco_${y}_${v}_b.data"
+		echo -n "" > "../data/quiosco_${y}_${v}_c.data"
+	done
 done
 
-for y in 1 5 10; do
-../bin/quiosco_exe 10 $y 100000 a 0 >> "../data/quiosco_${y}_a.data"
-../bin/quiosco_exe 10 $y 100000 b 0 >> "../data/quiosco_${y}_b.data"
-../bin/quiosco_exe 10 $y 100000 c 0 >> "../data/quiosco_${y}_c.data"
+for v in 1 100 1000 5000 10000 100000; do
+	for y in 1 5 10; do
+	../bin/quiosco_exe 10 $y $v a 0 >> "../data/quiosco_${y}_${v}_a.data"
+	../bin/quiosco_exe 10 $y $v b 0 >> "../data/quiosco_${y}_${v}_b.data"
+	../bin/quiosco_exe 10 $y $v c 0 >> "../data/quiosco_${y}_${v}_c.data"
+	done
 done
 
-for y in 1 5 10; do
-	echo -n "" > "../data/ganancia_${y}_a.data"
-	echo -n "" > "../data/ganancia_${y}_b.data"
-	echo -n "" > "../data/ganancia_${y}_c.data"
+for v in 1 100 1000 5000 10000 100000; do
+	for y in 1 5 10; do
+		echo -n "" > "../data/ganancia_${y}_${v}_a.data"
+		echo -n "" > "../data/ganancia_${y}_${v}_b.data"
+		echo -n "" > "../data/ganancia_${y}_${v}_c.data"
+	done
 done
 
-for y in 1 5 10; do
-	awk '{print $6}' <<< $(cat "../data/quiosco_${y}_a.data") >> "../data/ganancia_${y}_a.data"
-	awk '{print $6}' <<< $(cat "../data/quiosco_${y}_b.data") >> "../data/ganancia_${y}_b.data"
-	awk '{print $6}' <<< $(cat "../data/quiosco_${y}_c.data") >> "../data/ganancia_${y}_c.data"
+for v in 1 100 1000 5000 10000 100000; do
+	for y in 1 5 10; do
+		awk '{print $6}' <<< $(cat "../data/quiosco_${y}_${v}_a.data") >> "../data/ganancia_${y}_${v}_a.data"
+		awk '{print $6}' <<< $(cat "../data/quiosco_${y}_${v}_b.data") >> "../data/ganancia_${y}_${v}_b.data"
+		awk '{print $6}' <<< $(cat "../data/quiosco_${y}_${v}_c.data") >> "../data/ganancia_${y}_${v}_c.data"
+	done
 done
-
 gnuplot quiosco.gp
--- practica2/script/quiosco.gp
+++ practica2/script/quiosco.gp
@@ -1,20 +1,23 @@
 set terminal png size 960,540
 set yrange [0:*]
 
-set output "../memoria/img/distribucion_a.png"
-set title "Ganancia esperada para la distribución a"
-set xlabel "Periodicos contratados"
-set ylabel "Ganancia esperada"
-plot for [name in "1 5 10"] "../data/ganancia_".name."_a.data" with linespoints title "Ganancia para x = 10, y = ".name
 
-set output "../memoria/img/distribucion_b.png"
-set title "Ganancia esperada para la distribución b"
-set xlabel "Periodicos contratados"
-set ylabel "Ganancia esperada"
-plot for [name in "1 5 10"] "../data/ganancia_".name."_b.data" with linespoints title "Ganancia para x = 10, y = ".name
+do for [v in "1 100 1000 5000 10000 100000"]{
+	set output "../memoria/img/distribucion_a".v.".png"
+	set title "Ganancia esperada para la distribución a ".v." veces"
+	set xlabel "Periodicos contratados"
+	set ylabel "Ganancia esperada"
+	plot for [name in "1 5 10"] "../data/ganancia_".name."_".v."_a.data" with linespoints title "Ganancia para x = 10, y = ".name
 
-set output "../memoria/img/distribucion_c.png"
-set title "Ganancia esperada para la distribución c"
-set xlabel "Periodicos contratados"
-set ylabel "Ganancia esperada"
-plot for [name in "1 5 10"] "../data/ganancia_".name."_c.data" with linespoints title "Ganancia para x = 10, y = ".name
+	set output "../memoria/img/distribucion_b".v.".png"
+	set title "Ganancia esperada para la distribución b ".v." veces"
+	set xlabel "Periodicos contratados"
+	set ylabel "Ganancia esperada"
+	plot for [name in "1 5 10"] "../data/ganancia_".name."_".v."_b.data" with linespoints title "Ganancia para x = 10, y = ".name
+
+	set output "../memoria/img/distribucion_c".v.".png"
+	set title "Ganancia esperada para la distribución c con ".v." veces"
+	set xlabel "Periodicos contratados"
+	set ylabel "Ganancia esperada"
+	plot for [name in "1 5 10"] "../data/ganancia_".name."_".v."_c.data" with linespoints title "Ganancia para x = 10, y = ".name
+}
