c623ba9417635896213cb0b00cf1d6f6285f996b
Alexmnzlms <alexmanza2@gmail.com>
2020-10-28 18:14:48 +0100

    Extraidos maximos

--- practica2/script/ejecucion-quiosco.sh
+++ practica2/script/ejecucion-quiosco.sh
@@ -3,35 +3,41 @@
 cd ..
 make
 cd script
-for v in 1 100 1000 5000 10000 100000; do
+for v in 100 1000 5000 10000 100000; do
 	for y in 1 5 10; do
-		echo -n "" > "../data/quiosco_${y}_${v}_a.data"
-		echo -n "" > "../data/quiosco_${y}_${v}_b.data"
-		echo -n "" > "../data/quiosco_${y}_${v}_c.data"
+		for d in a b c; do
+			echo -n "" > "../data/quiosco_${y}_${v}_${d}.data"
+			echo -n "" > "../data/ganancia_${y}_${v}_${d}.data"
+			echo -n "" > "../data/max_ganancia_${y}_${v}_${d}.data"
+		done
 	done
 done
 
-for v in 1 100 1000 5000 10000 100000; do
+for v in 100 1000 5000 10000 100000; do
 	for y in 1 5 10; do
-	../bin/quiosco_exe 10 $y $v a 0 >> "../data/quiosco_${y}_${v}_a.data"
-	../bin/quiosco_exe 10 $y $v b 0 >> "../data/quiosco_${y}_${v}_b.data"
-	../bin/quiosco_exe 10 $y $v c 0 >> "../data/quiosco_${y}_${v}_c.data"
+		for d in a b c; do
+		../bin/quiosco_exe 10 $y $v $d 0 >> "../data/quiosco_${y}_${v}_${d}.data"
+		done
 	done
 done
 
-for v in 1 100 1000 5000 10000 100000; do
+for v in 100 1000 5000 10000 100000; do
 	for y in 1 5 10; do
-		echo -n "" > "../data/ganancia_${y}_${v}_a.data"
-		echo -n "" > "../data/ganancia_${y}_${v}_b.data"
-		echo -n "" > "../data/ganancia_${y}_${v}_c.data"
+		for d in a b c; do
+			awk '{print $6}' <<< $(cat "../data/quiosco_${y}_${v}_${d}.data") >> "../data/ganancia_${y}_${v}_${d}.data"
+		done
 	done
 done
 
-for v in 1 100 1000 5000 10000 100000; do
+for v in 100 1000 5000 10000 100000; do
 	for y in 1 5 10; do
-		awk '{print $6}' <<< $(cat "../data/quiosco_${y}_${v}_a.data") >> "../data/ganancia_${y}_${v}_a.data"
-		awk '{print $6}' <<< $(cat "../data/quiosco_${y}_${v}_b.data") >> "../data/ganancia_${y}_${v}_b.data"
-		awk '{print $6}' <<< $(cat "../data/quiosco_${y}_${v}_c.data") >> "../data/ganancia_${y}_${v}_c.data"
+		for d in a b c; do
+			awk 'END { printf "%d ", s} { max || max = $1; s || s = NR; if ($1 > max) {max=$1; s=NR} }' <<< $(cat "../data/ganancia_${y}_${v}_${d}.data") >> "../data/max_ganancia_${y}_${v}_${d}.data"
+			awk 'END { print max} { max || max = $1; s || s = NR; if ($1 > max) {max=$1; s=NR} }' <<< $(cat "../data/ganancia_${y}_${v}_${d}.data") >> "../data/max_ganancia_${y}_${v}_${d}.data"
+		done
 	done
 done
+
+rm ../data/*.data
+
 gnuplot quiosco.gp
--- practica2/script/quiosco.gp
+++ practica2/script/quiosco.gp
@@ -2,12 +2,12 @@ set terminal png size 960,540
 set yrange [0:*]
 
 
-do for [v in "1 100 1000 5000 10000 100000"]{
+do for [v in "100 1000 5000 10000 100000"]{
 	set output "../memoria/img/distribucion_a".v.".png"
 	set title "Ganancia esperada para la distribución a ".v." veces"
 	set xlabel "Periodicos contratados"
 	set ylabel "Ganancia esperada"
-	plot for [name in "1 5 10"] "../data/ganancia_".name."_".v."_a.data" with linespoints title "Ganancia para x = 10, y = ".name
+	plot for [name in "1 5 10"] "../data/ganancia_".name."_".v."_a.data" with linespoints title "Ganancia para x = 10, y = ".name, "../data/max_ganancia_1_".v."_a.data" with points title "Maximo"
 
 	set output "../memoria/img/distribucion_b".v.".png"
 	set title "Ganancia esperada para la distribución b ".v." veces"
