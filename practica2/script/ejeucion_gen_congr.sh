ea40483e56e24685b42e72b28bea94b51a7ef3c9
Alexmnzlms <alexmanza2@gmail.com>
2020-11-12 13:51:42 +0100

    Memoria IV fin

--- practica2/script/ejeucion_gen_congr.sh
+++ practica2/script/ejeucion_gen_congr.sh
@@ -5,22 +5,22 @@ make -B
 cd script
 
 for g in 2060 2061; do
-	for mod in 1 2 3 4; do
+	for mod in  1 2 3 4 5 6; do
 		echo -n "" > "../data/gcl/gen_congr_${g}_${mod}.data"
 		echo -n "" > "../data/gcl/max_gen_congr_${g}_${mod}.data"
 	done
 done
 
 for g in 2060 2061; do
-	for mod in 1 2 3 4; do
+	for mod in  1 2 3 4 5 6; do
 		for i in $(seq 0 1 9999); do
-			../bin/gen_congr_exe $g 4321 $i $mod >> "../data/gcl/gen_congr_${g}_${mod}.data"
+			../bin/gen_congr_exe $g 4321 $i $mod | tail -1 >> "../data/gcl/gen_congr_${g}_${mod}.data"
 		done
 	done
 done
 
 for g in 2060 2061; do
-	for mod in 1 2 3 4; do
+	for mod in  1 2 3 4 5 6; do
 		awk 'END { printf "%d ", s-1} { max || max = $2; s || s = NR; if ($2 > max) {max=$2; s=NR} }' <<< $(cat "../data/gcl/gen_congr_${g}_${mod}.data") >> "../data/gcl/max_gen_congr_${g}_${mod}.data"
 		awk 'END { print max} { max || max = $2; s || s = NR; if ($2 > max) {max=$2; s=NR} }' <<< $(cat "../data/gcl/gen_congr_${g}_${mod}.data") >> "../data/gcl/max_gen_congr_${g}_${mod}.data"
 	done
@@ -29,7 +29,7 @@ done
 echo -n "" > "../data/gcl/max_gen_congr.data"
 
 for g in 2060 2061; do
-	for mod in 1 2 3 4; do
+	for mod in  1 2 3 4 5 6; do
 		echo -n $g >> "../data/gcl/max_gen_congr.data"
 		echo -n " " >> "../data/gcl/max_gen_congr.data"
 		echo -n $mod >> "../data/gcl/max_gen_congr.data"
--- practica2/src/gen_congr.cpp
+++ practica2/src/gen_congr.cpp
@@ -12,13 +12,26 @@ int gen_congr_ae(int a, int x, int c, int m){
 	return (a*x + c) % m;
 }
 
-double gen_congr_ar(int a, double x, int c, int m){
+double gen_congr_ar_d(int a, double x, int c, int m){
 	x = (a*x + c) / m;
 	x = (x - (int)x) * m;
 	return x;
 }
 
-double gen_congr_arc(int a, double x, int c, int m){
+double gen_congr_arc_d(int a, double x, int c, int m){
+	x = (a*x + c) / m;
+	x = (x - (int)x) * m;
+	x = (int)(x+0.5);
+	return x;
+}
+
+float gen_congr_ar_f(int a, float x, int c, int m){
+	x = (a*x + c) / m;
+	x = (x - (int)x) * m;
+	return x;
+}
+
+float gen_congr_arc_f(int a, float x, int c, int m){
 	x = (a*x + c) / m;
 	x = (x - (int)x) * m;
 	x = (int)(x+0.5);
@@ -44,55 +57,50 @@ int main(int argc, char ** argv){
 	int x_inicial = atoi(argv[3]);
 	int metodo = atoi(argv[4]);
 
-	if(metodo > 4 || metodo < 1){
+	if(metodo > 6 || metodo < 1){
 		cerr << "Metodo incorrecto" << endl;
 		exit(1);
 	}
 
 	int periodo = -1;
+	int precision = 20;
 
+	double generado, generado_ant;
+	set<double> numeros;
 	if(metodo == 1){
-		int generado, generado_ant;
 		set<int> numeros;
-		bool terminado = false;
-		numeros.insert(x_inicial);
-		generado_ant = x_inicial;
-
-		while(!terminado && (int)numeros.size() < m){
-			generado = gen_congr_ae(a, generado_ant, c , m);
-			if(numeros.find(generado) != numeros.end()){
-				terminado = true;
-			} else {
-				numeros.insert(generado);
-				generado_ant = generado;
-			}
-			periodo = numeros.size();
-		}
-	} else {
-		double generado, generado_ant;
+	} else if(metodo == 2 || metodo == 4){
+		set<float> numeros;
+	} else if(metodo == 3 || metodo == 5 || metodo == 6){
 		set<double> numeros;
-		bool terminado = false;
-		numeros.insert(x_inicial);
-		generado_ant = x_inicial;
+	}
+	bool terminado = false;
+	numeros.insert(x_inicial);
+	generado_ant = x_inicial;
 
-		while(!terminado /*&& (int)numeros.size() < m*/){
-			if(metodo == 2){
-				generado = gen_congr_ar(a, generado_ant, c, m);
-			} else if (metodo == 3){
-				generado = gen_congr_arc(a, generado_ant, c, m);
-			} else {
-				generado = gen_congr_fmod(a, generado_ant, c, m);
-			}
-
-			if(numeros.find(generado) != numeros.end()){
-				terminado = true;
-			} else {
-				numeros.insert(generado);
-				cout << setprecision(15) << generado << "\t";
-				generado_ant = generado;
-			}
-			periodo = numeros.size();
+	while(!terminado && (int)numeros.size() < m){
+		if(metodo == 1){
+			generado = gen_congr_ae(a, generado_ant, c, m);
+		} else if(metodo == 2){
+			generado = gen_congr_ar_f(a, generado_ant, c, m);
+		} else if(metodo == 3){
+			generado = gen_congr_ar_d(a, generado_ant, c, m);
+		} else if (metodo == 4){
+			generado = gen_congr_arc_f(a, generado_ant, c, m);
+		} else if (metodo == 5){
+			generado = gen_congr_arc_d(a, generado_ant, c, m);
+		} else {
+			generado = gen_congr_fmod(a, generado_ant, c, m);
 		}
+
+		if(numeros.find(generado) != numeros.end()){
+			terminado = true;
+		} else {
+			numeros.insert(generado);
+			cout << setprecision(precision) << generado << "\t";
+			generado_ant = generado;
+		}
+		periodo = numeros.size();
 	}
 	cout << endl;
 	cout << "--------------------------" << endl;
