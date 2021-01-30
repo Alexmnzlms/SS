ec1370aaa6cbb3e8ac784b3252080e98ed3b757d
Alexmnzlms <alexmanza2@gmail.com>
2021-01-31 00:05:18 +0100

    Comprobación de parametros

--- ejercicios_examen/modelosdiscretos/src/simulacion_compania.cpp
+++ ejercicios_examen/modelosdiscretos/src/simulacion_compania.cpp
@@ -4,6 +4,12 @@
 using namespace std;
 
 int main(int argc, char** argv){
+
+	if(argc != 3){
+		cout << "Formato incorrecto: bin/simulacion_compania_exe n_sim mod" << endl;
+		exit(-1);
+	}
+
 	int sim = atoi(argv[1]);
 	int mod = atoi(argv[2]);
 
--- ejercicios_examen/montecarlo/src/simulacion_papel.cpp
+++ ejercicios_examen/montecarlo/src/simulacion_papel.cpp
@@ -5,6 +5,11 @@ using namespace std;
 
 int main(int argc, char** argv){
 
+	if(argc != 4){
+		cout << "Formato incorrecto: bin/simulacion_papel_exe n_sim p_obtenido p_total" << endl;
+		exit(-1);
+	}
+
 	int n_sim = atoi(argv[1]);
 	double p_obtenido = atof(argv[2]);
 	double p_total = atof(argv[3]);
